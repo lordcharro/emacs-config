87c19144dbbdfbd193617493118354bb2aa378e7
ivolopes <ilopes@openwt.com>
2022-08-23 15:30:25 +0200

    Add init.el and custom.el

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("fe1c13d75398b1c8fd7fdd1241a55c286b86c3e4ce513c4292d01383de152cb7" default))
+ '(package-selected-packages '(dracula-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,54 @@
+;; Org mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+;; key binding
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+
+;; no startup msg (this was preventing the default-directory)  
+(setq inhibit-startup-message t)
+
+;; Directories
+(setq org-directory (expand-file-name "~/org"))
+(setq default-directory "~/org/")
+; to open a default file at the beginning 
+;(setq org-default-notes-file (concat org-directory "/mygtd.org"))
+(setq org-agenda-files '("~/org" "~/www/org" "~/www/_org"))
+
+;; Templates
+;; using the "<P" shortcut 
+(require 'org-tempo)
+(tempo-define-template "project"
+		       '("PROJECT:" p n "DATE:" n "PM:" n)
+		       "<P"
+		       "Insert a project tempate")
+
+;; Add the Java structure code
+(add-to-list 'org-structure-template-alist'("j" . "src java"))
+
+;; TO-DOs
+(setq org-todo-keywords '((sequence "TODO(t)" "STARTED(s)" "BLOCKED(b)" "|" "DONE(d)" "CANCELED(c)")))
+
+(setq org-todo-keyword-faces
+      '(("BLOCKED" . (:foreground "OrangeRed" :weight bold))
+        ("STARTED" . (:foreground "SteelBlue" :weight bold))
+        ("CANCELED" . (:foreground "LimeGreen" :weight bold))
+        ))
+
+;; logging time when a to-do is marked as done
+(setq org-log-done 'time)
+
+;; to pass the custom config to a custom file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;;;;;;;;;;;
+;; Hooks
+;;;;;;;;;;;
+
+;; spell check
+(add-hook 'org-mode-hook 'flyspell-mode)
+
+;; allow line break at line x for org mode
+(add-hook 'org-mode-hook '(lambda () (setq fill-column 100)))
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
